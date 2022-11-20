ef4e770a51bd93f79a7cf5a3b1a31cd9d71648c4
Bibek-Bohora <bibchhetri5678@gmail.com>
2022-11-20 12:19:38 +0545

    iam rich app complets

--- udemy/lib/main.dart
+++ udemy/lib/main.dart
@@ -3,5 +3,68 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text("hello bibek")));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: IamRich()));
+}
+
+class IamRich extends StatelessWidget {
+  const IamRich({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: Text("I am rich"),
+        centerTitle: true,
+        actions: [
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Hello(),
+                  ));
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: Icon(Icons.settings),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Hello extends StatelessWidget {
+  const Hello({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 20, 130, 185),
+        title: Text("Setting"),
+        centerTitle: true,
+        actions: [
+          GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: Icon(Icons.home),
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: Container(
+          width: 100,
+          height: 500,
+          child: Text("hello from hello"),
+        ),
+      ),
+    );
+  }
 }
